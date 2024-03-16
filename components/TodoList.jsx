02b15ff3d73f1bfb118ec13901425daf0977d0f8
Bhargavaansh2211 <143218592+Bhargavaansh2211@users.noreply.github.com>
2024-03-16 12:59:02 +0530

    -

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { FlatList, View } from 'react-native';
 import Todo from './Todo';
 
-const TodoList = ({ todos, onPress, onCheckboxPress,onDelete }) => {
+const TodoList = ({ todos, onPress, onCheckboxPress }) => {
   return (
     <View>
       <FlatList
         data={todos}
         keyExtractor={(item) => (item.id ? item.id.toString() : Math.random().toString())}
         renderItem={({ item }) => (
-          <Todo todo={item} onPress={onPress} onCheckboxPress={onCheckboxPress} onDelete={onDelete}/>
+          <Todo todo={item} onPress={onPress} onCheckboxPress={onCheckboxPress} />
         )}
       />
     </View>
